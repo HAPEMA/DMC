be8d65ae1df71bc073b5e3bc10d45663b8829f5d
Hapema <130947283+HAPEMA@users.noreply.github.com>
2024-07-19 11:50:20 -0400

    DMC
    Version de 18/07/24

---
+++ Site.Master.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DMC
+{
+    public partial class SiteMaster : MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["NOMBRE"] != null)
+            {
+                string Nombre = Session["NOMBRE"].ToString();
+            }
+            if (Session["APPAT"] != null)
+            {
+                string APPAT = Session["APPAT"].ToString();
+            } 
+
+            
+            if (Session["ID_USUARIO"] != null)
+            {
+                string IDUsuario = Session["ID_USUARIO"].ToString();
+            }
+
+
+            string PrimerNombre = Session["NOMBRE"] as string;
+            string Apellido = Session["APPAT"] as string;
+
+            NombreyApellido.Text = PrimerNombre + " " + Apellido;
+        }
+        protected void BtnLogout_Click(object sender, EventArgs e)
+        {
+            // Limpiar la sesión
+            Session.Clear();
+            Session.Abandon();
+
+            // Desautenticar al usuario utilizando FormsAuthentication
+            FormsAuthentication.SignOut();
+
+            // Redirigir a la página de inicio de sesión
+            Response.Redirect("~/DMC/Login/Login.aspx");
+        }
+        protected void ddlProductosFx(object sender, EventArgs e)
+        {
+
+            if (ddlProductos.SelectedValue == "Inventario")
+            {
+                Response.Redirect("~/DMC/Productos/Inventario.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "Bodega")
+            {
+                Response.Redirect("~/DMC/Productos/Bodegas.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "IngresarMovimiento")
+            {
+                Response.Redirect("~/DMC/Productos/IngresarMovimiento.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "Documentos")
+            {
+                Response.Redirect("~/DMC/Productos/Documentos.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "MovimientoProductos")
+            {
+                Response.Redirect("~/DMC/Productos/MovimientoPorProducto.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "MaestroProductos")
+            {
+                Response.Redirect("~/DMC/Productos/MaestroProductos.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "AgregarProducto")
+            {
+                Response.Redirect("~/DMC/Productos/AgregarProducto.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "Familias")
+            {
+                Response.Redirect("~/DMC/Productos/Familias.aspx");
+            }
+            else if (ddlProductos.SelectedValue == "SubFamilias")
+            {
+                Response.Redirect("~/DMC/Productos/SubFamilias.aspx");
+            }
+
+        }
+        protected void ddlComercialFx(object sender, EventArgs e)
+        {
+            if (ddlComercial.SelectedValue == "MaestroCliente")
+            {
+                Response.Redirect("~/DMC/Comercio/MaestroCliente.aspx");
+            }
+            else if (ddlComercial.SelectedValue == "ListaPrecios")
+            {
+                Response.Redirect("~/DMC/Comercio/ListarPrecios.aspx");
+            }
+
+        }
+        protected void ddlDocumentoFx(object sender, EventArgs e)
+        {
+            if (ddlDocumento.SelectedValue == "EmitirDocumento")
+            {
+                Response.Redirect("~/DMC/Documentos/Cotizacion.aspx");
+            }
+            else if (ddlDocumento.SelectedValue == "ListadoDocumentos")
+            {
+                Response.Redirect("");
+            }
+        }
+        protected void ddlAdministrativoFx(object sender, EventArgs e)
+        {
+            if (ddlAdministrativo.SelectedValue == "Usuario")
+            {
+                Response.Redirect("~/DMC/Administracion/MantencionUsuario.aspx");
+            }
+        }
+        protected void ddlSoporteFx(object sender, EventArgs e)
+        {
+            if (ddlProductos.SelectedValue == "18")
+            {
+                Response.Redirect("~/DMC/Documentos/DesCotizacion.aspx");
+            }
+            else if (ddlAdministrativo.SelectedValue == "19")
+            {
+                Response.Redirect("~/DMC/Documentos/DesCotizacion.aspx");
+            }
+            else if (ddlAdministrativo.SelectedValue == "20")
+            {
+                Response.Redirect("~/DMC/Documentos/DesCotizacion.aspx");
+            }
+
+        }
+
+        protected void MenuInicio(object sender, EventArgs e)
+        {
+            Response.Redirect("~/DMC/Menus/MenuDespuesLogin.aspx");
+        }
+    }
+}
---
+++ Site.Master.designer.cs
@@ -0,0 +1,98 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace DMC
+{
+
+
+    public partial class SiteMaster
+    {
+
+        /// <summary>
+        /// Control Imglogo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ImageButton Imglogo;
+
+        /// <summary>
+        /// Control ddlProductos.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlProductos;
+
+        /// <summary>
+        /// Control ddlComercial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlComercial;
+
+        /// <summary>
+        /// Control ddlDocumento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlDocumento;
+
+        /// <summary>
+        /// Control ddlAdministrativo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlAdministrativo;
+
+        /// <summary>
+        /// Control ddlSoporte.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSoporte;
+
+        /// <summary>
+        /// Control NombreyApellido.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label NombreyApellido;
+
+        /// <summary>
+        /// Control btnLogout.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogout;
+
+        /// <summary>
+        /// Control MainContent.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+    }
+}
