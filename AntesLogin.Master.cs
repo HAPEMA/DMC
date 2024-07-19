7a1dc2d7dbbd124639c59c89b5485b04d4fdc574
Hapema <130947283+HAPEMA@users.noreply.github.com>
2024-07-19 11:48:51 -0400

    DMC
    Version dia 18/07/24

---
+++ AntesLogin.Master.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DMC
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnLogout_Click(object sender, EventArgs e)
+        {
+            // Limpiar la sesión
+            Session.Clear();
+            Session.Abandon();
+
+            // Desautenticar al usuario utilizando FormsAuthentication
+            FormsAuthentication.SignOut();
+
+            // Redirigir a la página de inicio de sesión
+            Response.Redirect("~/DMC/Login/Login.aspx");
+        }
+    }
+}
